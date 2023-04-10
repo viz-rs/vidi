d4eeeb52dc057e874acf49d1ede6d3b2c96f7222
Fangdun Tsai <cfddream@gmail.com>
2023-04-11 00:11:11 +0800

    test(core): Response::html

--- viz-core/tests/response.rs
+++ viz-core/tests/response.rs
@@ -1,15 +1,33 @@
 use headers::{ContentType, HeaderMapExt};
 use http_body_util::{BodyExt, Full};
 use hyper::body::Body;
-use viz_core::{OutgoingBody, Response, ResponseExt, Result};
+use viz_core::{OutgoingBody, Response, ResponseExt, Result, StatusCode};
 
 #[tokio::test]
 async fn response_ext() -> Result<()> {
-    let resp = Response::text(());
+    let resp = Response::with(Full::new("<xml/>".into()), mime::TEXT_XML.as_ref());
+
+    assert!(resp.ok());
 
     let content_type = resp.headers().typed_get::<ContentType>();
 
-    assert!(resp.ok());
+    assert_eq!(
+        Into::<mime::Mime>::into(content_type.unwrap()),
+        mime::TEXT_XML
+    );
+
+    let body: OutgoingBody = resp.into_body();
+
+    assert_eq!(body.size_hint().exact(), Some(b"<xml/>".len() as u64));
+    assert_eq!(body.collect().await.unwrap().to_bytes().to_vec(), b"<xml/>");
+
+    let mut resp = Response::text("");
+    *resp.status_mut() = StatusCode::NOT_FOUND;
+
+    assert!(!resp.ok());
+
+    let content_type = resp.headers().typed_get::<ContentType>();
+
     assert_eq!(
         Into::<mime::Mime>::into(content_type.unwrap()),
         mime::TEXT_PLAIN_UTF_8
@@ -21,20 +39,22 @@ async fn response_ext() -> Result<()> {
     assert!(body.frame().await.is_none());
     assert!(body.is_end_stream());
 
-    let resp = Response::with(Full::new("<xml/>".into()), mime::TEXT_XML.as_ref());
+    let resp = Response::html("<html/>");
+
+    assert!(resp.ok());
 
     let content_type = resp.headers().typed_get::<ContentType>();
 
-    assert!(resp.ok());
     assert_eq!(
         Into::<mime::Mime>::into(content_type.unwrap()),
-        mime::TEXT_XML
+        mime::TEXT_HTML_UTF_8
     );
 
-    let body: OutgoingBody = resp.into_body();
+    let mut body: OutgoingBody = resp.into_body();
 
-    assert_eq!(body.size_hint().exact(), Some(b"<xml/>".len() as u64));
-    assert_eq!(body.collect().await.unwrap().to_bytes().to_vec(), b"<xml/>");
+    assert_eq!(body.size_hint().exact(), Some(7));
+    assert_eq!(body.frame().await.unwrap().unwrap().into_data().unwrap(), &"<html/>"[..]);
+    assert!(body.is_end_stream());
 
     Ok(())
 }
