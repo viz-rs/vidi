4434643a677f8a0f3ffb4fdc210edf6fb55cd1d4
Fangdun Tsai <fundon@pindash.io>
2024-01-01 13:39:25 +0800

    chore(core): remove redundance bounds on Cloneable

--- viz-core/src/handler/cloneable.rs
+++ viz-core/src/handler/cloneable.rs
@@ -1,7 +1,6 @@
 use super::Handler;
 
-pub(crate) type BoxCloneable<Input, Output> =
-    Box<dyn Cloneable<Input, Output = Output> + Send + Sync + 'static>;
+pub(crate) type BoxCloneable<Input, Output> = Box<dyn Cloneable<Input, Output = Output>>;
 
 pub(crate) trait Cloneable<Input>: Handler<Input> {
     fn clone_box(&self) -> BoxCloneable<Input, Self::Output>;
@@ -9,7 +8,7 @@ pub(crate) trait Cloneable<Input>: Handler<Input> {
 
 impl<Input, T> Cloneable<Input> for T
 where
-    T: Handler<Input> + Send + Sync + Clone + 'static,
+    T: Handler<Input> + Clone,
 {
     fn clone_box(&self) -> BoxCloneable<Input, Self::Output> {
         Box::new(self.clone())
